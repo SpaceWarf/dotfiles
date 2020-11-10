63177082f5d1d1e5f7978f087ae0f8db75ffdc3c
SpaceWarf <space.warf@gmail.com>
2020-11-10 18:43:33 -0500

    Update make-new-env.sh

--- make-new-env.sh
+++ make-new-env.sh
@@ -1,14 +1,10 @@
 sudo apt update &&\
   sudo apt upgrade -y &&\
-  sudo apt install -y git tmux zsh python-pip python-dev python3-dev
+  sudo apt install -y git tmux zsh
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
 
 cp .zshrc ~
 cp .tmux.conf ~
-cp ys.zsh-theme ~/.oh-my-zsh/custom/themes
-
-mkdir -p ~/.tmux/plugins
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp bureau-custom.zsh-theme ~/.oh-my-zsh/custom/themes
